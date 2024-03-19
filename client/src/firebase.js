dd0e3519731c02e1e4d1d8554f3eee1e166957b2
Vaish0202 <vaishnavilikhe2798@gmail.com>
2024-03-19 08:54:59 +0530

    complete profile page ui

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "eliteestate-c4850.firebaseapp.com",
   projectId: "eliteestate-c4850",
   storageBucket: "eliteestate-c4850.appspot.com",
